d25c8adb3a102e3bd83316ecb0036c9ab9bba982
Amr AbulAzm <amrabulazm@Amrs-Laptop.local>
2024-06-12 12:26:33 +0200

    fix: possible img load issues; import; load mechanism

--- src/components/CanvasWithImage/CanvasWithImage.tsx
+++ src/components/CanvasWithImage/CanvasWithImage.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { rgbToHex } from '../../utils/RgbToHex'
+import BeachImage from '../../components/CanvasWithImage/beach.jpg'
 
 interface CanvasWithImageProps {
   toggleDropperOnCanvas: () => void
@@ -16,7 +17,30 @@ const CanvasWithImage: React.FC<CanvasWithImageProps> = ({
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null)
   const imgRef = useRef<HTMLImageElement | null>(null)
 
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   useEffect(() => {
+    const img = imgRef.current || new Image()
+    imgRef.current = img
+
+    img.onload = () => {
+      setImageLoaded(true)
+    }
+
+    // Set the image source only if it hasn't been set already to avoid repeated loading
+    if (!img.src) {
+      img.src = BeachImage;
+    }
+
+    return () => {
+      // Clear the onload handler to prevent memory leaks
+      img.onload = null;
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!imageLoaded || !imgRef.current) return
+
     const canvas = canvasRef.current
     if (!canvas) return
 
@@ -25,18 +49,13 @@ const CanvasWithImage: React.FC<CanvasWithImageProps> = ({
 
     ctxRef.current = ctx // cached to avoid repeated creation
 
-    const img = imgRef.current || new Image()
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-    }
-    img.src = 'src/components/CanvasWithImage/beach.jpg'
-    imgRef.current = img // cached to avoid re-creation on subsequent renders
+    ctx.drawImage(imgRef.current, 0, 0, canvas.width, canvas.height)
 
     return () => {
       // cleanup to avoid memory leaks
       ctx.clearRect(0, 0, canvas.width, canvas.height)
     }
-  }, [])
+  }, [imageLoaded])
 
   const onMouseMoveHandler = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const canvas = canvasRef.current
